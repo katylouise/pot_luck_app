86fd018866af31c9c1941d1030953e5cefd4e4dc
giuseppe de santis <giusdesan@gmail.com>
2015-11-17 20:53:56 +0000

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name swFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the swFrontApp
+ */
+angular.module('swFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name swFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the swFrontApp
+ */
+angular.module('swFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
