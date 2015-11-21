4d222fe90db9d1d9ae0fa995863e7ff7f4b7d8ba
giuseppe de santis <giusdesan@gmail.com>
2015-11-21 13:55:00 +0000

    created navbar directive with active links

---
+++ app/scripts/directives/navbar.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('swFrontApp').directive('navbar', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'views/navbar.html',
+    controller: 'NavigationController'
+  }
+});
