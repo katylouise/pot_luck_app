88351305bdd2350e3f1c0d67c2e57b336b6d19f6
giuseppe de santis <giusdesan@gmail.com>
2015-11-21 12:42:16 +0000

    merged middleware in one function and front-end is finally talking to backend

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,37 +79,34 @@ module.exports = function (grunt) {
         hostname: 'localhost',
         livereload: 35729
       },
-      proxies: [
-        {
-          context: 'api',
+      proxies: [{
+          context: '/api',
           host: 'localhost',
           port: 3000
-        }
-      ],
+        }],
       livereload: {
         options: {
-          middleware: function (connect, options) {
-                        if (!Array.isArray(options.base)) {
-                            options.base = [options.base];
-                        }
-
-                        // Setup the proxy
-                        var middlewares = [require('grunt-connect-proxy/lib/utils').proxyRequest];
-
-                        // Serve static files.
-                        options.base.forEach(function(base) {
-                            middlewares.push(connect.static(base));
-                        });
-
-                        // Make directory browse-able.
-                        var directory = options.directory || options.base[options.base.length - 1];
-                        middlewares.push(connect.directory(directory));
-
-                        return middlewares;
-                    },
           open: true,
-          middleware: function (connect) {
+
+          middleware: function (connect, options) {
+            if (!Array.isArray(options.base)) {
+              options.base = [options.base];
+            }
+
+            // Setup the proxy
+            var middlewares = [require('grunt-connect-proxy/lib/utils').proxyRequest];
+
+            // Serve static files.
+            options.base.forEach(function(base) {
+              middlewares.push(connect.static(base));
+            });
+
+            // Make directory browse-able.
+            var directory = options.directory || options.base[options.base.length - 1];
+            middlewares.push(connect.directory(directory));
+
             return [
+              middlewares,
               connect.static('.tmp'),
               connect().use(
                 '/bower_components',
@@ -515,8 +512,8 @@ module.exports = function (grunt) {
     'clean:server',
     'wiredep',
     'concurrent:test',
-    'postcss',
     'configureProxies:server',
+    'postcss',
     'connect:test',
     'karma'
   ]);
