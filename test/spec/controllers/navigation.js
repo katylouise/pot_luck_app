0a229a84e28d051a124969603653046bc80bfd6a
giuseppe de santis <giusdesan@gmail.com>
2015-11-22 20:34:56 +0000

    added first test for NavigationController, passing

--- test/spec/controllers/navigation.js
+++ test/spec/controllers/navigation.js
@@ -2,5 +2,21 @@
 
 describe('Controller: NavigationController', function () {
 
+  var location, scope;
+
+  beforeEach(module('swFrontApp'));
+
+  beforeEach(inject(function($controller, $rootScope, $location){
+    location = $location;
+    scope = $rootScope.$new();
+    $controller('NavigationController', { $scope: scope })
+  }));
+
+  describe('isActive', function() {
+    it('returns true when paths are the same', function() {
+      location.path('/test');
+      expect(scope.isActive('/test')).toBeTruthy();
+    });
+  });
 
 });
