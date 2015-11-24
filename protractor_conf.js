6ccd7d4125f211c74133ba55b914019d2d3c05b1
giuseppe de santis <giusdesan@gmail.com>
2015-11-24 16:08:38 +0000

    finished setting up protractor

--- protractor_conf.js
+++ protractor_conf.js
@@ -1,6 +1,9 @@
 // An example configuration file.
 exports.config = {
-  directConnect: true,
+  chromeOnly: true,
+  chromeDriver: './node_modules/protractor/selenium/chromedriver',
+
+  baseUrl: 'http://localhost:9000/',
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
@@ -8,14 +11,15 @@ exports.config = {
   },
 
   // Framework to use. Jasmine is recommended.
-  framework: 'jasmine',
+  // framework: 'jasmine',
 
   // Spec patterns are relative to the current working directly when
   // protractor is called.
-  specs: ['example_spec.js'],
+  specs: ['test/e2e/**/*_spec.js'],
 
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
+    showColors: true,
     defaultTimeoutInterval: 30000
   }
 };
