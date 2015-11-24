7153c95113141e863ada4fa65a0e1649b286ac6b
giuseppe de santis <giusdesan@gmail.com>
2015-11-24 16:45:26 +0000

    added more e2e protractor tests

--- protractor_conf.js
+++ protractor_conf.js
@@ -3,6 +3,8 @@ exports.config = {
   chromeOnly: true,
   chromeDriver: './node_modules/protractor/selenium/chromedriver',
 
+  // seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+
   baseUrl: 'http://localhost:9000/',
 
   // Capabilities to be passed to the webdriver instance.
--- test/e2e/navigation_spec.js
+++ test/e2e/navigation_spec.js
@@ -8,5 +8,11 @@ describe('Navigation menu', function() {
     var text = activeListItem.element(by.tagName('a')).getText();
 
     expect(text).toEqual('Home');
+
+    var edgesLink = element(by.linkText('Edges'));
+    edgesLink.click();
+    activeListItem = element(by.css('.active'));
+    text = activeListItem.element(by.tagName('a')).getText();
+    expect(text).toEqual('Edges');
   });
 });
