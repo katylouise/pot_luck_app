cde87a8e77562fdd27eaf671fb01bfe4fc724584
Rebecca Appleyard <rklappleyard@gmail.com>
2015-12-05 21:15:42 +0000

    Ingredients loading correctly with shops

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,6 +6,9 @@ angular.module('swFrontApp')
     $scope.ingredients = [];
 
     var people;
+    var shops;
+    var shopsArr;
+
     $scope.findPerson = function() {
       people = $http({method: 'GET', url: '/data/people.json'});
       people.then(function(success) {
@@ -20,8 +23,17 @@ angular.module('swFrontApp')
       $scope.people = [];
     }
 
-    $scope.addIngredient = function() {
-      $scope.ingredients.push($scope.ingredientSearchTerm);
+    $scope.addIngredientsAndShopToList = function() {
+      shops = $http({method: 'GET', url: '/data/shops.json'});
+      shops.then(function(success) {
+        $scope.shop = success.data.filter(function(shop) {
+          return shop.items.indexOf($scope.ingredientSearchTerm) > -1;
+        });
+        if ($scope.shop.length === 0) {
+          $scope.shop = [{ "name" : "Not in database - find it yourself!"} ];
+        }
+        $scope.ingredients.push({ "ingredient": $scope.ingredientSearchTerm, "shop": $scope.shop[0] });
+      });
     }
 
   });
