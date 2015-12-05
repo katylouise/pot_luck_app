127aadefa5557d6986dabbd84177c78b6df48085
Rebecca Appleyard <rklappleyard@gmail.com>
2015-12-05 20:30:38 +0000

    Shopping list adds ingredients

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,9 @@
 
 angular.module('swFrontApp')
   .controller('MainCtrl', function($scope, $http) {
+    $scope.partyGuests = [];
+    $scope.ingredients = [];
+
     var people;
     $scope.findPerson = function() {
       people = $http({method: 'GET', url: '/data/people.json'});
@@ -10,6 +13,15 @@ angular.module('swFrontApp')
           return person.name.toLowerCase() === $scope.friendSearchTerm.toLowerCase();
         });
       });
+    }
+
+    $scope.addPersonToList = function(person) {
+      $scope.partyGuests.push(person);
+      $scope.people = [];
+    }
+
+    $scope.addIngredient = function() {
+      $scope.ingredients.push($scope.ingredientSearchTerm);
     }
 
   });
