10da5c786bf5bf69f90075ac498ca77cfe572966
giuseppe de santis <giusdesan@gmail.com>
2015-12-05 22:23:02 +0000

    guests showing on map :)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,6 +5,45 @@ angular.module('swFrontApp')
     $scope.partyGuests = [];
     $scope.ingredients = [];
 
+    require([
+      "esri/map",
+      "esri/graphic",
+      "esri/layers/GraphicsLayer",
+      "esri/geometry/Point",
+      "esri/symbols/SimpleMarkerSymbol",
+      "esri/InfoTemplate",
+      "dojo/domReady!"
+      ],
+      everythingElse)
+
+      function everythingElse(Map, Graphic, GraphicsLayer, Point, SimpleMarkerSymbol, InfoTemplate) {
+        var map = new Map("map", {
+          basemap: "gray",
+          center: [0,51.5], // longitude, latitude
+          zoom: 12
+        });
+        var locationLayer = new GraphicsLayer();
+        // var point = new Point(0,51.5);
+        // var symbol = new SimpleMarkerSymbol().setColor("#1036DE").setSize(14);
+        // var graphic = new Graphic(point, symbol);
+        // var infoTemplate = new InfoTemplate();
+        // locationLayer.add(graphic);
+        // graphic.setInfoTemplate(infoTemplate);
+        // map.addLayer(locationLayer);  // Makes sure that map is loaded
+
+
+    function drawOnMap(array){
+          for(var i=0; i<array.length; i++){
+            var longitude = array[i].coords[1];
+            var latitude = array[i].coords[0];
+            var point = new Point(longitude, latitude);
+            var symbol = new SimpleMarkerSymbol().setColor("#1036DE").setSize(14);
+            var graphic = new Graphic(point, symbol);
+            locationLayer.add(graphic);
+            console.log("I am working");
+          }
+          map.addLayer(locationLayer);
+        }
     var people;
     $scope.findPerson = function() {
       people = $http({method: 'GET', url: '/data/people.json'});
@@ -18,10 +57,12 @@ angular.module('swFrontApp')
     $scope.addPersonToList = function(person) {
       $scope.partyGuests.push(person);
       $scope.people = [];
+      drawOnMap($scope.partyGuests);
     }
 
     $scope.addIngredient = function() {
       $scope.ingredients.push($scope.ingredientSearchTerm);
     }
 
-  });
+  }
+})
