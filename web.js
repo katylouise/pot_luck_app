e46b12cc82b29b74899d288331e4c019b9d8684f
Diego Regules <diegoregules@gmail.com>
2015-12-05 19:49:22 +0000

    sending twilio token to client implemented

--- web.js
+++ web.js
@@ -2,11 +2,24 @@
 
 var gzippo = require('gzippo');
 var express = require('express');
+var app = express();
 var morgan = require('morgan');
 var twilio = require('twilio');
 var appPort = process.env.PORT || 5000;
 
-var app = express();
+var accountSID = 'AC3dc935f68825d2ac5df7a3d874331784';
+var authToken = 'e91eec8358b643b6e16fa52e77b78b0d';
+var appSid = "APd0c95b98dbdf101aa8fa8be315e1e683";
+// var
+app.get("/twilio/token", function(req, res) {
+  var capability = new twilio.Capability (accountSID, authToken);
+  capability.allowClientOutgoing(appSid);
+
+  res.send(capability.generate());
+
+});
+
+
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
