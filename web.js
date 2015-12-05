05dd82c8975b1d12f85d659b3111b64ad90356a9
Diego Regules <diegoregules@gmail.com>
2015-12-05 18:38:11 +0000

    added twilio dependency

--- web.js
+++ web.js
@@ -1,8 +1,15 @@
+'use strict';
+
 var gzippo = require('gzippo');
 var express = require('express');
 var morgan = require('morgan');
+var twilio = require('twilio');
+var appPort = process.env.PORT || 5000;
+
 var app = express();
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.listen(appPort, function() {
+  console.log("App server started and listening at:", appPort);
+});
