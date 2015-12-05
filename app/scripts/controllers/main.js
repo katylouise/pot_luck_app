4368ce69d576862444ad30207c5feec67060fc19
Rebecca Appleyard <rklappleyard@gmail.com>
2015-12-05 19:52:56 +0000

    Finding friends successfully

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,8 +6,10 @@ angular.module('swFrontApp')
     $scope.findPerson = function() {
       people = $http({method: 'GET', url: '/data/people.json'});
       people.then(function(success) {
-        console.log(success.data);
-        $scope.person = success.data;
+        $scope.people = success.data.filter(function(person) {
+          return person.name.toLowerCase() === $scope.friendSearchTerm.toLowerCase();
+        });
       });
     }
+
   });
