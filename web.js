3f2bf40335613d91d9e966f734ce109796a03b27
giuseppe de santis <giusdesan@gmail.com>
2015-12-05 17:58:24 +0000

    added morgan

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var gzippo = require('gzippo');
-  var express = require('express');
-  var app = express();
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
 
-  app.use(express.logger('dev'));
-  app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-  app.listen(process.env.PORT || 5000);
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
